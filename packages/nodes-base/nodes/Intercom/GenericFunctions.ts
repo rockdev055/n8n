9542b504a99da88decd5a82e56300739d03b426b
rockdev55 <steve264225@gmail.com>
2019-11-21 12:39:04 -0500

    :sparkles: done

--- packages/nodes-base/nodes/Intercom/GenericFunctions.ts
+++ packages/nodes-base/nodes/Intercom/GenericFunctions.ts
@@ -7,37 +7,30 @@ import {
 	IExecuteSingleFunctions
 } from 'n8n-core';
 
-export async function mailchimpApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, endpoint: string, method: string, body: any = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
-	const credentials = this.getCredentials('mailchimpApi');
-
+export async function intercomApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, resource: string, method: string, body: any = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
+	const credentials = this.getCredentials('intercomApi');
 	if (credentials === undefined) {
 		throw new Error('No credentials got returned!');
 	}
 
-	const headerWithAuthentication = Object.assign({}, headers, { Authorization: `apikey ${credentials.apiKey}` });
+	const headerWithAuthentication = Object.assign({}, headers,
+		{ Authorization: `Bearer ${credentials.apiKey}`, Accept: 'application/json' });
 
-	if (!(credentials.apiKey as string).includes('-')) {
-		throw new Error('The API key is not valid!');
-	}
-
-	const datacenter = (credentials.apiKey as string).split('-').pop();
-
-	const host = 'api.mailchimp.com/3.0';
+	const endpoint = 'api.intercom.io';
 
 	const options: OptionsWithUri = {
 		headers: headerWithAuthentication,
 		method,
-		uri: `https://${datacenter}.${host}${endpoint}`,
-		json: true,
+		uri: `https://${endpoint}${resource}`,
+		body,
+		json: true
 	};
-
-	if (Object.keys(body).length !== 0) {
-		options.body = body;
-	}
 
 	try {
 		return await this.helpers.request!(options);
 	} catch (error) {
+		console.error(error);
+
 		const errorMessage = error.response.body.message || error.response.body.Message;
 
 		if (errorMessage !== undefined) {
