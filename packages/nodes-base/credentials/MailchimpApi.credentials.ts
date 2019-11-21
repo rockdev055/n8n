cc1378ce006cd92915453dabc6ada220f04c7694
rockdev55 <steve264225@gmail.com>
2019-11-22 00:07:38 +0100

    :zap: Support also numbers as values in options and multiOptions

--- packages/nodes-base/credentials/MailchimpApi.credentials.ts
+++ packages/nodes-base/credentials/MailchimpApi.credentials.ts
@@ -3,10 +3,9 @@ import {
 	NodePropertyTypes,
 } from 'n8n-workflow';
 
-
-export class IntercomApi implements ICredentialType {
-	name = 'intercomApi';
-	displayName = 'Intercom API';
+export class MailchimpApi implements ICredentialType {
+	name = 'mailchimpApi';
+	displayName = 'Mailchimp API';
 	properties = [
 		{
 			displayName: 'API Key',
--- packages/nodes-base/nodes/Mailchimp/GenericFunctions.ts
+++ packages/nodes-base/nodes/Mailchimp/GenericFunctions.ts
@@ -7,30 +7,37 @@ import {
 	IExecuteSingleFunctions
 } from 'n8n-core';
 
-export async function intercomApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, resource: string, method: string, body: any = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
-	const credentials = this.getCredentials('intercomApi');
+export async function mailchimpApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, endpoint: string, method: string, body: any = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
+	const credentials = this.getCredentials('mailchimpApi');
+
 	if (credentials === undefined) {
 		throw new Error('No credentials got returned!');
 	}
 
-	const headerWithAuthentication = Object.assign({}, headers,
-		{ Authorization: `Bearer ${credentials.apiKey}`, Accept: 'application/json' });
+	const headerWithAuthentication = Object.assign({}, headers, { Authorization: `apikey ${credentials.apiKey}` });
 
-	const endpoint = 'api.intercom.io';
+	if (!(credentials.apiKey as string).includes('-')) {
+		throw new Error('The API key is not valid!');
+	}
+
+	const datacenter = (credentials.apiKey as string).split('-').pop();
+
+	const host = 'api.mailchimp.com/3.0';
 
 	const options: OptionsWithUri = {
 		headers: headerWithAuthentication,
 		method,
-		uri: `https://${endpoint}${resource}`,
-		body,
-		json: true
+		uri: `https://${datacenter}.${host}${endpoint}`,
+		json: true,
 	};
+
+	if (Object.keys(body).length !== 0) {
+		options.body = body;
+	}
 
 	try {
 		return await this.helpers.request!(options);
 	} catch (error) {
-		console.error(error);
-
 		const errorMessage = error.response.body.message || error.response.body.Message;
 
 		if (errorMessage !== undefined) {
