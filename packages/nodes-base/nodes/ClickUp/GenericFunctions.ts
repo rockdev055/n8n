f662c48c88d918f6a6a84cd3b9664ee6948a4220
rockdev55 <steve264225@gmail.com>
2020-02-06 21:32:55 -0800

    :shirt: Fix lint issue

--- packages/nodes-base/nodes/ClickUp/GenericFunctions.ts
+++ packages/nodes-base/nodes/ClickUp/GenericFunctions.ts
@@ -54,13 +54,3 @@ export async function clickupApiRequestAllItems(this: IHookFunctions | IExecuteF
 	);
 	return returnData;
 }
-
-export function validateJSON(json: string | undefined): any { // tslint:disable-line:no-any
-	let result;
-	try {
-		result = JSON.parse(json!);
-	} catch (exception) {
-		result = undefined;
-	}
-	return result;
-}
