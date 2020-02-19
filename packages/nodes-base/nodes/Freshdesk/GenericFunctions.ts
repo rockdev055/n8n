f8d84e16c40e1cb6dfe16cccd9444a4e608b0601
rockdev55 <steve264225@gmail.com>
2020-02-19 11:29:03 -0600

    :zap: Add execution url on Error-Workflow trigger

--- packages/nodes-base/nodes/Freshdesk/GenericFunctions.ts
+++ packages/nodes-base/nodes/Freshdesk/GenericFunctions.ts
@@ -2,13 +2,15 @@ import { OptionsWithUri } from 'request';
 
 import {
 	IExecuteFunctions,
+	IHookFunctions,
 	ILoadOptionsFunctions,
+	IExecuteSingleFunctions,
 	BINARY_ENCODING
 } from 'n8n-core';
 
-import { IDataObject } from 'n8n-workflow';
+import * as _ from 'lodash';
 
-export async function freshdeskApiRequest(this: IExecuteFunctions | ILoadOptionsFunctions, method: string, resource: string, body: any = {}, query: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+export async function freshdeskApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, resource: string, method: string, body: any = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
 
 	const credentials = this.getCredentials('freshdeskApi');
 
@@ -18,56 +20,34 @@ export async function freshdeskApiRequest(this: IExecuteFunctions | ILoadOptions
 
 	const apiKey = `${credentials.apiKey}:X`;
 
-	const endpoint = 'freshdesk.com/api/v2';
+	const headerWithAuthentication = Object.assign({}, headers, { Authorization: `${Buffer.from(apiKey).toString(BINARY_ENCODING)}` });
 
-	let options: OptionsWithUri = {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `${Buffer.from(apiKey).toString(BINARY_ENCODING)}`,
-		},
+	const endpoint = 'freshdesk.com/api/v2/';
+
+	const options: OptionsWithUri = {
+		headers: headerWithAuthentication,
 		method,
 		body,
-		qs: query,
-		uri: uri || `https://${credentials.domain}.${endpoint}${resource}`,
+		uri: `https://${credentials.domain}.${endpoint}${resource}`,
 		json: true
 	};
-	if (!Object.keys(body).length) {
+
+	if (_.isEmpty(options.body)) {
 		delete options.body;
 	}
-	if (!Object.keys(query).length) {
-		delete options.qs;
-	}
-	options = Object.assign({}, options, option);
+
 	try {
 		return await this.helpers.request!(options);
 	} catch (error) {
-		console.log(error.response.body)
-		const errorMessage = error.response.body.message || error.response.body.Message || error.response.body.description;
+		console.error(error);
+
+		const errorMessage = error.response.body.message || error.response.body.Message;
+
 		if (errorMessage !== undefined) {
-			throw new Error(errorMessage);
-		};
-		return new Error(error)
-	}
-}
-
-export async function freshdeskApiRequestAllItems(this: IExecuteFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-
-	const returnData: IDataObject[] = [];
-
-	let responseData;
-	let uri: string | undefined;
-	query.per_page = 100;
-	do {
-		responseData = await freshdeskApiRequest.call(this, method, endpoint, body, query, uri, { resolveWithFullResponse: true });
-		if (responseData.headers.link) {
-			uri = responseData.headers['link'].split(';')[0].replace('<', '').replace('>','');
+			throw errorMessage;
 		}
-		returnData.push.apply(returnData, responseData.body);
-	} while (
-		responseData.headers['link'] !== undefined &&
-		responseData.headers['link'].includes('rel="next"')
-	);
-	return returnData;
+		throw error.response.body;
+	}
 }
 
 export function validateJSON(json: string | undefined): any { // tslint:disable-line:no-any
