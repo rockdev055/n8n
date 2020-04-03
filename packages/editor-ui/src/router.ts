7ee5be2217d3aac941f11f19ad9ddc9a501d9549
rockdev55 <steve264225@gmail.com>
2020-04-03 11:48:34 +0200

    :zap: Small adjustment on Github Credentials

--- packages/editor-ui/src/router.ts
+++ packages/editor-ui/src/router.ts
@@ -20,12 +20,6 @@ export default new Router({
 			},
 		},
 		{
-			path: '/oauth2/callback',
-			name: 'oAuth2Callback',
-			components: {
-			},
-		},
-		{
 			path: '/workflow',
 			name: 'NodeViewNew',
 			components: {
