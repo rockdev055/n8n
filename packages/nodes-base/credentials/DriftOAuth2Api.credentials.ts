ac4a4f3ec9dde30cfe66d8744376778dc4a484d2
rockdev55 <steve264225@gmail.com>
2020-06-28 11:30:26 -0400

    :zap: Add option to ignore SSL issues to Graph API node (#703)

--- packages/nodes-base/credentials/DriftOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/DriftOAuth2Api.credentials.ts
@@ -3,40 +3,45 @@ import {
 	NodePropertyTypes,
 } from 'n8n-workflow';
 
-export class ZoomOAuth2Api implements ICredentialType {
-	name = 'zoomOAuth2Api';
-	extends = ['oAuth2Api'];
-	displayName = 'Zoom OAuth2 API';
+
+export class DriftOAuth2Api implements ICredentialType {
+	name = 'driftOAuth2Api';
+	extends = [
+		'oAuth2Api',
+	];
+	displayName = 'Drift OAuth2 API';
 	properties = [
 		{
 			displayName: 'Authorization URL',
 			name: 'authUrl',
 			type: 'hidden' as NodePropertyTypes,
-			default: 'https://zoom.us/oauth/authorize'
+			default: 'https://dev.drift.com/authorize',
+			required: true,
 		},
 		{
 			displayName: 'Access Token URL',
 			name: 'accessTokenUrl',
 			type: 'hidden' as NodePropertyTypes,
-			default: 'https://zoom.us/oauth/token'
-		},
-		{
-			displayName: 'Scope',
-			name: 'scope',
-			type: 'hidden' as NodePropertyTypes,
-			default: ''
+			default: 'https://driftapi.com/oauth2/token',
+			required: true,
 		},
 		{
 			displayName: 'Auth URI Query Parameters',
 			name: 'authQueryParameters',
 			type: 'hidden' as NodePropertyTypes,
-			default: ''
+			default: '',
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden' as NodePropertyTypes,
+			default: '',
 		},
 		{
 			displayName: 'Authentication',
 			name: 'authentication',
 			type: 'hidden' as NodePropertyTypes,
-			default: 'header'
-		}
+			default: 'body',
+		},
 	];
 }
